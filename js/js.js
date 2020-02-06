25ae80aae139a79f8b472c956526397d1a83237f
MiguelAngelGarre <magarreacebes@gmail.com>
2020-02-06 22:24:31 +0100

    jsModificadoProbandoExtensionVSC

--- js/js.js
+++ js/js.js
@@ -1,23 +1,6 @@
 var listElements = document.querySelector(".contenedor");
-//var numPokemons;
-//function countPokemons() {
-//    let request = new XMLHttpRequest();
-//   request.open('GET', 'https://pokeapi.co/api/v2/pokemon');
-//  request.responseType = 'json';
-//request.onreadystatechange = function () {
-//  console.log('ReadyState: ' + request.readyState);
-//console.log('Status: ' + request.status);
-//if (request.readyState === 4 && request.status === 200) {
-//  console.log(request.response);
-//var response = request.response;
-//numPokemons = response.count;
-//listElements.innerHTML += `<li>${numPokemons}</li>`;
-//}
-//};
-//request.send();
-//}
 function fetchItems() {
-    for (let i = 1; i <= 151; i++) {
+    for (let i = 1; i <= 150; i++) {
         let request = new XMLHttpRequest();
         request.open('GET', `https://pokeapi.co/api/v2/pokemon/${i}`);
         request.responseType = 'json';
@@ -41,5 +24,4 @@ function fetchItems() {
         request.send();
     }
 }
-//countPokemons();
 fetchItems();
